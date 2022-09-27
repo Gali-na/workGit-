f61be32c512e914503d5207598ba8849eb17456a
Gali-na <galinatsivka@gmail.com>
2022-09-27 12:56:27 +0300

    initial commit

---
+++ src/s01/task_01.java
@@ -0,0 +1,10 @@
+package s01;
+
+public class task_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(" I try to use git");
+        System.out.println(" I try to use git");
+        System.out.println(" I try to use git");
+    }
+}
