0d9427a424a89557072385bd4ecc3e42e063cfe0
Gali-na <galinatsivka@gmail.com>
2022-09-27 16:13:43 +0300

    Work in main

--- src/s01/task_01.java
+++ src/s01/task_01.java
@@ -6,5 +6,6 @@ public class task_01 {
         System.out.println(" I try to use git");
         System.out.println(" I try to use git");
         System.out.println(" I try to use java");
+        System.out.println(" Work in main");
     }
 }
